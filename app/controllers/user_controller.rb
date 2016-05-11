70e4a4c2acf5888f49588d8d1b7d9e0a6a8802e3
hassan-maqsood <hassanch5757@gmail.com>
2016-05-12 00:34:40 +0500

    user controller added + routes defined

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,11 @@
+# require 'test_helper'
+class UserController < ApplicationController
+  def new
+  end
+
+  # test 'should get new' do
+  #   get :new
+  #   assert_response   :success
+  # end
+
+end
