3b902f9a1f1a1664de142c2f4f4499727c18a8b1
hassan-maqsood <hassanch5757@gmail.com>
2016-05-12 00:33:57 +0500

    user controller added + routes defined

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,12 @@ class PagesController < ApplicationController
 
   def help
   end
+
+  def about
+
+  end
+
+  def contact
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  get 'user/new'
 
-  get 'pages/help'
+  # get 'pages/home'
+  #
+  # get 'pages/help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
+  # root 'pages#home'
+
+  root             'pages#home'
+  get 'help'    => 'pages#help'
+  get 'about'   => 'pages#about'
+  get 'contact' => 'pages#contact'
+  get 'signup' => 'user#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
