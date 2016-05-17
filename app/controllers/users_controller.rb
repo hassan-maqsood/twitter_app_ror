60ff5d5837e44cc79cbf97e79b41868d15e1ad7b
hassan-maqsood <hassanch5757@gmail.com>
2016-05-18 00:48:19 +0500

    update,listing,deleting-by-admin-done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,33 @@
 # require 'test_helper'
 class UsersController < ApplicationController
+
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in first."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 
   # test 'should get new' do
@@ -36,6 +62,12 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User has been deleted."
+    redirect_to users_url
   end
 
   private
