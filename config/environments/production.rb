c812fe2454692197d6f457b99ddc8b1c2fcfac57
hassan-maqsood <hassanch5757@gmail.com>
2016-05-20 00:14:02 +0500

    email-conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
       :address        => 'smtp.gmail.com',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['gmail_username'],
-      :password       => ENV['gmail_password'],
+      :user_name      => 'hassan221b@gmail.com',
+      :password       => 'googlenewhtc221b',
       :enable_starttls_auto => true
   }
 end
